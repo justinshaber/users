ca45baf7830daf67f47d1a16d40b2d0ac5a8e79e
Justin Shaber <justinshaber@gmail.com>
2024-03-11 16:55:34 -0600

    created bio page for each user that displays email and interests

--- users.rb
+++ users.rb
@@ -7,6 +7,35 @@ before do
   @users = YAML.load_file('users.yml')
 end
 
+not_found do 
+  redirect "/"
+end
+
+helpers do
+  def format_interests(interests)
+    interests.join(", ")
+  end
+end
+
+def user_data(user)
+  name = user.to_s.capitalize
+  email = @users[user][:email]
+  interests = @users[user][:interests]
+
+  [name, email, interests]
+end
+
 get "/" do
   erb :home
 end
+
+get "/:user" do
+  @user = params[:user].to_sym
+  redirect "/" unless @users.has_key? @user
+  @name, @email, @interests = user_data(@user)
+
+  erb :user_bio
+end
+
+
+
